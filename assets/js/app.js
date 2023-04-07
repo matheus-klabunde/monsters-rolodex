c5776cd1cfd0d35abcb9442d2b0e476b20c2ac95
Matheus Klabunde <119515341+matheus-klabunde@users.noreply.github.com>
2023-04-07 14:25:28 -0400

    Code optimization and html correction

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,30 +2,17 @@ const urlApi = 'https://jsonplaceholder.typicode.com/users';
 const input = document.querySelector('.search');
 const section = document.querySelector('.card-list');
 
-async function noSearch(){
-  const response = await fetch(urlApi)
-  const data = await response.json()
-
-  const filteredMonsters = data.filter(data => data);
-    
-  const card = term =>{
-    return `
-    <div>
-      <img src='https://robohash.org/${term.id}?set=set2&size=180x180' alt='monster ${term.id}'>
-      <h2> ${term.name}</h2>
-      <p>${term.email}</p>
-    </div>
-    `
-  }
-  section.innerHTML = filteredMonsters.map(users => card(users)).join('');
-}
-
 async function search(el){
-  const response = await fetch(urlApi)
-  const data = await response.json()
+  const request = await fetch(urlApi)
+  const data = await request.json()
+  let filteredMonsters;
 
-  const filteredMonsters = data.filter(data => data.name.toLowerCase().includes(el.toLowerCase()));
-    
+  if(!el){
+    filteredMonsters = data.filter(data => data);
+  } else {
+    filteredMonsters = data.filter(data => data.name.toLowerCase().includes(el.toLowerCase()));
+  }
+  
   const card = term =>{
     return `
     <div>
@@ -45,10 +32,10 @@ input.addEventListener('keyup', e => {
     search(el);
     return;
   }
-  noSearch();
+  search();
 });
 
-noSearch();
+search();
 
 
 
